68e06faffd29804c662b710d8a39ccf48b38899e
Victor Fernandes <victorfariafernandes@gmail.com>
2026-04-28 14:37:38 -0300

    refactor: group lssvm package
    Move lssvm_*, fhe_inference, qr_householder_plain, fhe_solvers/ under
    lssvm/. Drop example_solver.py (no external refs). Add package __init__.

---
+++ lssvm/solvers/__init__.py
@@ -0,0 +1,13 @@
+from . import cg_cipher
+from . import qr_householder_cipher_cell
+from . import qr_householder_cipher_col
+from . import qr_householder_cipher_row
+from . import utils
+
+__all__ = [
+    "cg_cipher",
+    "qr_householder_cipher_cell",
+    "qr_householder_cipher_col",
+    "qr_householder_cipher_row",
+    "utils",
+]
